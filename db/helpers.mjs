19a07f26ed440f2ac818bb606b5270b4411feac1
Maruf Hasan <maruf15-8118@diu.edu.bd>
2023-11-28 06:02:08 +0600

    file is formated

--- db/helpers.mjs
+++ db/helpers.mjs
@@ -12,24 +12,24 @@ function getWordList() {
     return new Promise((res, rej) => {
 
         readStream.on('end', function () {
- questionList = sanitizeWordList(data);
+            questionList = sanitizeWordList(data);
             res(questionList)
         })
     });
-    
-function sanitizeWordList(wordListString) {
-    return JSON.parse(wordListString);
-}
 
-//    function sanitizeWordList(wordListString) {
-//        return wordListString
-//            .split('\n')
-//            .filter((listEntry) => listEntry.length > 0)
-//            .map((listEntry) => listEntry.split(',')
-//                .map((v, i) => i !== 0 ? Number(v) : v))
-//            .map((v) => {return {question: v[0],ans: v[0], attempt: v[1], successRate: v[2]}})
-//            .sort(questionSortFunction)
-//    }
+    function sanitizeWordList(wordListString) {
+        return JSON.parse(wordListString);
+    }
+
+    //    function sanitizeWordList(wordListString) {
+    //        return wordListString
+    //            .split('\n')
+    //            .filter((listEntry) => listEntry.length > 0)
+    //            .map((listEntry) => listEntry.split(',')
+    //                .map((v, i) => i !== 0 ? Number(v) : v))
+    //            .map((v) => {return {question: v[0],ans: v[0], attempt: v[1], successRate: v[2]}})
+    //            .sort(questionSortFunction)
+    //    }
 }
 
 
@@ -39,8 +39,8 @@ function writingToFile(filePath) {
 
             const writeStream = createWriteStream(filePath, {flags: flag});
             const contentString = JSON.stringify(content);
-                writeStream.write(contentString);
-/*
+            writeStream.write(contentString);
+            /*
             content.forEach(word => {
 
                 writeStream.write(word.content+ ',' + word.attempt + ',' + word.successRate + '\n');
